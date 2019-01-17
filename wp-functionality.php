cb6b8544bd1834e44ddc18788e9e2b17f50b1190
John Bunka <JohnBunka@users.noreply.github.com>
2019-01-17 13:40:35 -0700

    Create wp-functionality.php

---
+++ wp-functionality.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * Plugin Name: WP Functionality
+ * Description: This contains all your site's core functionality so that it is theme independent. <strong>It should always be activated</strong>.
+ * Version:     1.0.0
+ * Author:      John Bunka & Press Avenue
+ *
+ * This program is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License version 3, as published by the
+ * Free Software Foundation.  You may NOT assume that you can use any other
+ * version of the GPL.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.
+ *
+ * @package    WPFunctionality
+ * @since      1.0.0
+ * @copyright  Copyright (c) 2019, John Bunka & Press Avenue
+ * @license    GPL-2.0+
+ */
+ 
+ // Insert WP Functionlity Below
